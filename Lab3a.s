d1f73e95712882644edfe3e68b31c16d8b3f487a
Andrewrockss <37053355+Andrewrockss@users.noreply.github.com>
2018-03-27 12:15:15 -0600

    Update Lab3a.s

--- Lab3a.s
+++ Lab3a.s
@@ -22,72 +22,69 @@ sub.l #40, %sp
 movem.l %d2-%d7/%a2-%a5, (%sp)
 clr.l %d2 			/*%d2 = storage*/
 
-jsr cr
-jsr cr
-
-pea Welcome
-jsr iprintf
-addq.l #4,%sp
-
-jsr cr
-pea Entries
-jsr iprintf
-addq.l #4, %sp
-
-jsr cr
-jsr getstring
-move.l %d0, %d2
-jsr cr
+jsr cr      	/*new line*/
+jsr cr        	/*new line*/
 
 
-EntryCheck:			
-cmp.l #3, %d2
-blt InvalidESma
-cmp.l #15, %d2
-bgt InvalidEBig
-bra ContinueEnt
+pea WelcomePrompt       	/*Push welcome messadge onto the stack*/
+jsr iprintf         	/*print the messadge to Matty*/
+addq.l #4,%sp         	/*clean up the stack*/
+
+jsr cr      	/*new line*/
+pea Entries      /*Push Entry messadge onto the stack*/
+jsr iprintf     *print the messadge to Matty*/
+addq.l #4, %sp     /*clean up the stack*/
+
+jsr cr      	/*new line*/
+jsr getstring   /*get the number the user typed */
+move.l %d0, %d2 /*move the number the useer entered to d2 */
+jsr cr  	/*new line*/
 
 
-InvalidEBig:
-pea InvalidBigS
-jsr iprintf			
-move.l %d2, (%sp)
-jsr value
-addq.l #4, %sp 		
+CheckEntry: 	/*testing the user's entry*/
+cmp.l #3, %d2  /*compareing with 3*/
+blt InvalidESma   /*branch to Invalid_tosmall if less than 3*/
+cmp.l #15, %d2    /*compareing with 15*/
+bgt Invalid_tobig    /*branch to Invalid_tobig if greater than 3*/
+bra ContinueEnt /*branch to ContinueEnt if the value is greater than */
 
-jsr cr
-jsr getstring
-move.l %d0, %d2
-jsr cr
-bra EntryCheck
+Invalid_tobig: 
+pea Invalid_tobig    /*push the error messadge to stack an ask for new one */
+jsr iprintf			    /*print the messadge to Matty*/
+move.l %d2, (%sp)    /*move d2 into stack pointer*/
+jsr value              /*print the value on the stack to Matty*/
+addq.l #4, %sp 		 /*clean up the stack*/
 
-InvalidESma:
-pea InvalidSmaS
-jsr iprintf			
-move.l %d2, (%sp)
-jsr value
-addq.l #4, %sp 		
+jsr cr    /*new line*/
+jsr getstring     /*get the number the user typed */
+move.l %d0, %d2    /*print the value on the stack to Matty*/
+jsr cr        	/*new line*/
+bra CheckEntry   /*testing the user's entry*/
 
-jsr cr
-jsr getstring
-move.l %d0, %d2
-jsr cr
-bra EntryCheck
+Invalid_tosmall:
+pea InvalidSmaS   /*push the error messadge to stack an ask for new one */
+jsr iprintf			/*print the messadge to Matty*/
+move.l %d2, (%sp)  /*move d2 into stack pointer*/
+jsr value         /*print the value on the stack to Matty*/
+addq.l #4, %sp 		/*clean up the stack*/
 
-ContinueEnt:
+jsr cr          /*new line*/
+jsr getstring    /*get the number the user typed */
+move.l %d0, %d2     /*move the value the user entered to d2*/
+jsr cr           	/*new line*/
+bra EntryCheck   /*testing the user's entry in d2*/
+   
+ContinueEnt:     /*branch to ContinueEnt if the value is in range */
+move.l %d2, 48(%sp) /*move d2 to a number of entereies located on the stack*/
+jsr cr     	/*new line*/
+pea Divisor    /*push onto stack the messadge to get Divisor */
+jsr iprintf       /*print the messadge to Matty*/
+addq.l #4, %sp    /*clean up the stack*/
 
-move.l %d2, 48(%sp)
-
-
-jsr cr
-pea Divisor
-jsr iprintf
-addq.l #4, %sp
-
-jsr cr
-jsr getstring
-move.l %d0, %d2
-jsr cr
+jsr cr       	/*new line*/
+jsr getstring     /*get the number the user typed */
+move.l %d0, %d2  
+jsr cr    	/*new line*/
 
 
 DivisorCheck:			
@@ -103,9 +100,9 @@ pea InvalidSmaS
 jsr iprintf			
 move.l %d2, (%sp)
 jsr value
-addq.l #4, %sp
+addq.l #4, %sp     /*clean up the stack*/
 jsr cr
-jsr getstring
+jsr getstring      /*get the number the user typed */
 move.l %d0, %d2
 jsr cr
 bra DivisorCheck
@@ -113,13 +110,13 @@ bra DivisorCheck
 
 InvalidDBig:
 pea InvalidBigS
-jsr iprintf			
+jsr iprintf			/*print the messadge to Matty*/
 move.l %d2, (%sp)
 jsr value
-addq.l #4, %sp 		
+addq.l #4, %sp 	 /*clean up the stack*/	
 
 jsr cr
-jsr getstring
+jsr getstring     /*get the number the user typed */
 move.l %d0, %d2
 jsr cr
 bra DivisorCheck
@@ -136,21 +133,20 @@ move.l #0x2300000, %a2
 ValueLoop:
 jsr cr
 pea EnterNum
-jsr iprintf
-addq.l #4, %sp
+jsr iprintf   /*print the messadge to Matty*/
+addq.l #4, %sp /*clean up the stack*/
 
-jsr getstring
+jsr getstring     /*get the number the user typed */
 move.l %d0, %d2
 cmp.l #0, %d2
 bge Continue
 jsr cr
 pea InvalidLT0
-jsr iprintf
+jsr iprintf   /*print the messadge to Matty*/
 addq.l #4, %sp
 bra ValueLoop
 
 Continue:
-
 move.l %d2, (%a2)+
 subq.l #1, %d7
 cmp.l #1, %d7
@@ -158,29 +154,29 @@ bgt ValueLoop
 Next:
 jsr cr			
 pea FinalNum
-jsr iprintf
+jsr iprintf   /*print the messadge to Matty*/
 addq.l #4, %sp
 
-jsr getstring
+jsr getstring     /*get the number the user typed */
 cmp.l #0, %d0
 ble Next
 move.l %d0, (%a2)+
 
-movem.l (%sp), %d2-%d7/%a2-%a5
-add.l #40, %sp
+movem.l (%sp), %d2-%d7/%a2-%a5  
+add.l #40, %sp      /*clean up the stack*/
 
 rts
 
 /*End of Subroutine **************************************************/ 
 .data
 
-Welcome:
+WelcomePrompt:
 .string "Welcome to Wing's statistics program"
 Entries:
 .string "Please enter the number(3min-15max) of entries followed by 'enter'"
-InvalidBigS:
+Invalid_tobig:
 .string "Invalid entry. Please enter a number less than "
-InvalidSmaS:
+Invalid_tosmall:
 .string "Invalid entry. Please enter a number more than "
 Divisor:
 .string "Please enter the divisor(2min-5max) followed by 'enter'"
