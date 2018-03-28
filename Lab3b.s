8d013087c70e8bb46d0980227748508674eae433
Andrewrockss <37053355+Andrewrockss@users.noreply.github.com>
2018-03-27 23:12:36 -0600

    Update Lab3b.s

--- Lab3b.s
+++ Lab3b.s
@@ -26,67 +26,72 @@ sub.l #40, %sp
 movem.l %d2-%d7/%a2-%a5, (%sp)
 clr %d4	
 
-move.l 48(%sp), %d7/*Counter MinMax*/
-move.l %a2, %a4	/*Copy array pointer*/
-move.l (%a4), %d2 /*Min*/
-move.l (%a4), %d3/*Max*/
+move.l 48(%sp), %d7    /*Counter MinMax*/
+move.l %a2, %a4      	/*Copy array pointer*/
+move.l (%a4), %d2     /*Min inital*/
+move.l (%a4), %d3     /*Max inital*/
 
-MinMax:
-cmp.l 4(%a4), %d2
-blt Min
-move.l 4(%a4), %d2
+MinMax:        /*Min,Max* calculation*/
+cmp.l 4(%a4), %d2    /*compare the next value stored to the current min*/
+blt Min               /*if next value is less than current min branch to min*/
+move.l 4(%a4), %d2      /*if not min put value in d2*/
+
 Min:
-cmp.l 4(%a4), %d3
-bgt Max
-move.l 4(%a4), %d3
-Max:
-subq.l #1, %d7
-addq.l #4, %a4
-cmp.l #1, %d7
-bgt MinMax
+cmp.l 4(%a4), %d3    /*compare the next value stored to the current max*/
+bgt Max              /*if next value is greater than current max branch to max*/
+move.l 4(%a4), %d3  /*move value to d3*/
 
-move.l %d2, (%a3)+			
+Max:
+subq.l #1, %d7   /*subtract from counter*/
+addq.l #4, %a4    /*increment entery being checked*/
+cmp.l #1, %d7   /*go to next number if counter is not zero*/
+bgt MinMax    
+
+
+move.l %d2, (%a3)+			/*result of mix and max gose into a3*/
 move.l %d3, (%a3)+			
 
-move.l 48(%sp), %d7			
-move.l %a2, %a4				
-clr %d2						
+move.l 48(%sp), %d7			 /*Counter once agian*/
+move.l %a2, %a4				/*result of mix and max gose into a4*/
+clr %d2						  /*clearing d2*/
 
 Mean:
-add.l (%a4)+, %d2
-subq.l #1, %d7
-tst %d7
-bne Mean
-divu.l 48(%sp), %d2
-move.w #0, (%a3)+
-move.w %d2, (%a3)+			
+add.l (%a4)+, %d2  /*adding all the enterys adn stroring reult into d2*/
+subq.l #1, %d7 /*minus from the counter */
+tst %d7    /*comparing counter with zero*/
+bne Mean  /*go to Mean if counter is not zero*/
+
+divu.l 48(%sp), %d2  /*divide d2 but the total number of enteries */
+move.w #0, (%a3)+    /*clear world length */
+move.w %d2, (%a3)+			/*store answer in a3*/
 
 
-move.l 48(%sp), %d7			
-move.l %a2, %a4				
-clr %d2						
+move.l 48(%sp), %d7			/*Counter once agian*/
+move.l %a2, %a4				/*Copy array pointer*/
+clr %d2						/*Clearing d2*/
 
 Divisor:
-tst %d7
-beq End
-subq.l #1, %d7
+tst %d7   /*Comparison with zero*/
+beq End   /*end if counter is zero*/
+subq.l #1, %d7   /*decresed the counter*/
 
-move.l (%a4)+, %d2
-divu.w 46(%sp), %d2
-swap %d2
-tst.w %d2
-bne Divisor
-move.l -4(%a4), (%a3)+
-addq.l #1, %d4
-bra Divisor
+move.l (%a4)+, %d2    /*move fist value to d2*/
+divu.w 46(%sp), %d2    /*divide it by the divisor*/
+swap %d2        /*swap upper and lower halves*/
+tst.w %d2     /*Comparison with zero*/
+bne Divisor      /*do agian if not equal to zer0*/
+move.l -4(%a4), (%a3)+ /*divisable if equal to zero, move answer to a3*/
+addq.l #1, %d4   /*add one to indicate how many are divisable*/
+bra Divisor     /*check the next value*/
 
-End:
+End:  
 
-move.l %d4, 52(%sp)
-movem.l (%sp), %d2-%d7/%a2-%a5
-add.l #40, %sp
+move.l %d4, 52(%sp)   /*how many divisable enteries*/
+movem.l (%sp), %d2-%d7/%a2-%a5   /*restore all the used registers*/ 
+add.l #40, %sp      /*clean up the stack*/
 
-rts 
+
+rts    /*return from the stack, and pop the value of the return addresss*/ 
 
 /*End of Subroutine **************************************************/ 
 .data
